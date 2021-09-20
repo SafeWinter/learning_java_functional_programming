877e3c1079d9984c20769a0801f4afc0bf5ac965
=SafeWinter <zandong_19@aliyun.com>
2021-09-20 20:52:20 +0800

    Added curry and lambda expression revisited demo

---
+++ src/fp/ch2/ex2/Demo17_Currying.java
@@ -0,0 +1,27 @@
+package fp.ch2.ex2;
+
+import java.util.function.BiFunction;
+import java.util.function.Function;
+
+public class Demo17_Currying {
+	
+	public static void main(String[] args) {
+		BiFunction<String, String, String> fn = (a, b) -> a + b;
+		System.out.println(fn.apply("Cat", "Dog"));  // CatDog
+		
+		Function<String, Function<String, String>> intermediateFunction;
+		intermediateFunction = a -> b -> fn.apply(a, b);
+		
+		Function<String, String> curryCatFn = intermediateFunction.apply("Cat ");
+		System.out.println(curryCatFn);
+		
+		System.out.println(curryCatFn.apply("Dog"));
+		
+		//----------------
+		
+		Function<Double, Function<Double, Double>> curryAdd = a -> b -> a + b;
+		Function<Double, Double> plus3 = curryAdd.apply(3.0d);
+		Double result = plus3.apply(4.0d);
+		System.out.println(result);  // 7.0
+	}
+}
---
+++ src/fp/ch2/ex2/Demo18_LambdaExpressionRevisited.java
@@ -0,0 +1,46 @@
+package fp.ch2.ex2;
+
+import java.util.function.BiConsumer;
+import java.util.function.BiFunction;
+import java.util.function.Consumer;
+import java.util.function.Function;
+
+public class Demo18_LambdaExpressionRevisited {
+	
+	public static void main(String[] args) {
+		
+		Function<Integer, Integer> demo1;
+		
+		demo1 = x -> x + 1;
+				
+		demo1 = (x) -> x + 1;
+		
+		demo1 = (Integer x) -> x + 1;
+
+		demo1 = x -> {
+		    x = x + 1;
+		    return x;
+		};
+		
+		demo1 = x -> {
+			Integer y;
+			y = x + 1;
+			return y;
+		};
+		
+		
+		BiFunction<Integer, Integer, Integer> demo2;
+		
+		demo2 = (x, y) -> x + y;
+		
+		demo2 = (Integer x, Integer y) -> x + y;
+		
+		
+		BiConsumer<Integer, Integer> demo3;
+		
+		demo3 = (x, y) -> System.out.println(x + y);
+		
+		
+
+	}
+}
