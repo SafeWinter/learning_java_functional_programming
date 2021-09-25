74b0642b7453c0e479099d21bee97bc36fecddf7
=SafeWinter <zandong_19@aliyun.com>
2021-09-25 23:39:49 +0800

    Renamed the demo name to be more meaningful and straightforward

--- src/ch4/ex2/Demo06_FilterAnimals.java
+++ src/ch4/ex2/Demo06_FilterAnimals.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Stream;
 
-public class Demo06_filter {
+public class Demo06_FilterAnimals {
 
     public static void main(String[] args) {
         String[] animals = {"cats", "dog", "ox", "bats", "horses", "mule"};
