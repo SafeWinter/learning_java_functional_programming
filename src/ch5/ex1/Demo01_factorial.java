00160b5f163b1d6829721a08779e9ff938938e23
=SafeWinter <zandong_19@aliyun.com>
2021-09-27 23:54:53 +0800

    Added Demo01 in ch5.ex1 on factorial example.

---
+++ src/ch5/ex1/Demo01_factorial.java
@@ -0,0 +1,25 @@
+package ch5.ex1;
+
+public class Demo01_factorial {
+
+    public static void main(String[] args) {
+        // Iterative approach
+        long fact = 1;
+        for(int i = 1; i <= 5; i++) {
+            fact *= i;
+        }
+        System.out.printf("fact: %d\n", fact);
+        
+        // Recursive approach
+        long factNew = factorial(5);
+        System.out.printf("fact: %d\n", factNew);
+    }
+
+    private static long factorial(int n) {
+        if(n == 1) {
+            return 1;
+        } else {
+            return n * factorial(n - 1);
+        }
+    }
+}
