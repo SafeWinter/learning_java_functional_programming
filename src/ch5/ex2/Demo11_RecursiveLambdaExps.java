d1a3f020b0d57f2502dfdce0da1fab3ce782cd03
=SafeWinter <zandong_19@aliyun.com>
2021-09-29 01:50:00 +0800

    Added Demo11 on recursive lambda expression. Pay attention to the initialization of lambda expression, using static field.

---
+++ src/ch5/ex2/Demo11_RecursiveLambdaExps.java
@@ -0,0 +1,32 @@
+package ch5.ex2;
+
+import java.util.function.Consumer;
+
+import ch5.ex1.Node;
+
+public class Demo11_RecursiveLambdaExps {
+
+    private static Consumer<Node> inorder;
+
+    public static void main(String[] args) {
+
+        inorder = node -> {
+            if(node == null) {
+                return;
+            }
+            inorder.accept(node.left());
+            System.out.printf("%d ", node.getValue());
+            inorder.accept(node.right());
+        };
+        
+        Node root = getRootNode();
+        inorder.accept(root);
+    }
+    
+    private static Node getRootNode() {
+        Node root = new Node(12);
+        root.addLeft(8).addRight(9);
+        root.addRight(18).addLeft(14).addRight(17);
+        return root;
+    }
+}
