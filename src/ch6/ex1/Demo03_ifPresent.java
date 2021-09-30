e96e3805b10b366ef33a8587c0f51621bc8a4365
E14 <zhouandong@sinosoft.com.cn>
2021-09-30 15:20:34 +0800

    Added Demo04 on orElse methods in ch6.ex1

---
+++ src/ch6/ex1/Demo03_ifPresent.java
@@ -0,0 +1,47 @@
+package ch6.ex1;
+
+public class Demo03_ifPresent {
+
+	public static void main(String[] args) {
+		
+		Customer defaultCustomer = new Customer(0, "Default");
+		
+		Customers customers = new Customers()
+				.addCustomer(123, new Customer(123, "Sue"))
+				.addCustomer(456, new Customer(456, "Bob"))
+				.addCustomer(789, new Customer(789, "Mary"))
+				.addCustomer(defaultCustomer.getId(), defaultCustomer);
+		
+		int id = 456;
+		
+		// orElse:
+		Customer current = customers.findOptionalCustomerWithID(id)
+			.orElse(defaultCustomer);
+		System.out.println(current);
+		
+		
+		// orElseGet:
+		current = customers.findOptionalCustomerWithID(id)
+			.orElseGet(() -> defaultCustomer);
+		System.out.println(current);
+		
+		
+		// orElseThrow:
+		try {
+			current = customers.findOptionalCustomerWithID(id)
+				.orElseThrow(NoCustomerFoundException::new);
+			System.out.println(current);
+		} catch (NoCustomerFoundException e) {
+			// Handle exception
+			String errMsg = "Exception: " + e.getLocalizedMessage();
+			System.out.println(errMsg);
+		}
+	}
+}
+
+class NoCustomerFoundException extends Exception {
+	public NoCustomerFoundException() { super(); }
+	public NoCustomerFoundException(String errMsg) {
+		super(errMsg);
+	}
+}
