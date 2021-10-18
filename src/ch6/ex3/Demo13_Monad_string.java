40ccc9311e0d0fb3543453610eca8b491f747321
E14 <zhouandong@sinosoft.com.cn>
2021-10-18 14:09:32 +0800

    Added Demo13 on Monad string composition example in ch6.ex3.

---
+++ src/ch6/ex3/Demo13_Monad_string.java
@@ -0,0 +1,22 @@
+package ch6.ex3;
+
+import java.util.Optional;
+import java.util.function.Function;
+
+import javax.security.auth.x500.X500Principal;
+
+public class Demo13_Monad_string {
+	public static void main(String[] args) {
+		Function<? super String, Optional<String>> toConcatString = x -> Optional.of(x + "ing");
+		Function<? super String, Optional<String>> toUpperString = x -> Optional.of(x.toUpperCase());
+		
+		// execution order not interchangeable
+		Optional<String> ostring = Optional.of("go");
+		Optional<String> result = ostring
+			.flatMap(toConcatString)
+			.flatMap(toUpperString);
+			
+		System.out.println(ostring.get());  // go
+		System.out.println(result.get());  // GOING
+	}
+}
