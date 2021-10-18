2fafeeb7fe3ebd3b0358b44acc06ebe655370f69
=SafeWinter <zandong_19@aliyun.com>
2021-10-19 00:19:15 +0800

    Added Demo03 on log and duration computation composition.

---
+++ src/ch7/ex1/Demo03_ExecuteAround_log_duration.java
@@ -0,0 +1,38 @@
+package ch7.ex1;
+
+import java.util.Arrays;
+import java.util.function.Function;
+import java.util.stream.IntStream;
+
+public class Demo03_ExecuteAround_log_duration {
+
+	public static void main(String[] args) {
+		
+	    Function<Integer, Integer> computeSquare = n -> n * n;
+	    Function<Integer, Integer> computeDuration = 
+	            x -> executeDuration(computeSquare, x);
+	            
+	    Integer[] arr = {1, 2, 3, 4, 5};
+	    Arrays.stream(arr)
+	        .map(computeDuration)
+//	        .map(x->executeDuration(computeSquare, x))
+	        .forEach(System.out::println);
+	}
+	
+	
+    public static int executeDuration(Function<Integer, Integer> computation, int value) {
+        long start = System.currentTimeMillis();
+        int result = 0;
+        try {
+            result = computation.apply(value);
+            Thread.sleep(500);
+        } catch (InterruptedException ex) {
+            ex.printStackTrace();
+        }
+        long end = System.currentTimeMillis();
+        long duration = end - start;
+        System.out.print("Duration: " + duration + " - ");
+        return result;
+    }
+	
+}
