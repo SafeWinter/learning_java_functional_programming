2841e7fece68002a659658729f167cd20105f7a2
SafeWinter <zandong_19@aliyun.com>
2021-10-21 22:25:57 +0800

    Added Demo04 on recursive lambda expression in debugging in ch8.ex2

---
+++ src/ch8/ex2/Demo04_Debug_recursive_lambda.java
@@ -0,0 +1,35 @@
+package ch8.ex2;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class Demo04_Debug_recursive_lambda {
+	
+	private static Consumer<Node> inorder;
+	
+    public static void main(String[] args) {
+    	
+    	Node root = getRootNode();
+    	
+    	inorder = (Node node) -> {
+    		if(node == null) {
+    			return;
+    		} else {
+				inorder.accept(node.left());
+				System.out.print(node.getValue() + " ");
+				inorder.accept(node.right());
+			}
+    	};
+    	
+    	inorder.accept(root);
+    }
+
+	private static Node getRootNode() {
+		Node root = new Node(12);
+	    root.addLeft(8).addRight(9);
+	    root.addRight(18).addLeft(14).addRight(17);
+		return root;
+	}
+    
+}
