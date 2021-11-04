b7facdd378e20bb6736cc6187c21e3336bc82104
andong2007 <andong_2007@qq.com>
2021-11-04 18:34:05 +0800

    Rearrange the code sequence of Location.java in ch9.ex1.

--- src/ch9/ex1/Location.java
+++ src/ch9/ex1/Location.java
@@ -40,52 +40,52 @@ public class Location {
         this.items.add(item);
     }
 
-    public void addNPC(String npc) {
+    public List<String> getNPCs() {
+	    return npcs;
+	}
+
+	public void addNPC(String npc) {
         this.npcs.add(npc);
     }
 
-    public List<String> getNPCs() {
-        return npcs;
-    }
+    public Optional<String> getLocation(String direction) {
+	    if (this.directions.containsKey(direction)) {
+	        return Optional.of(this.directions.get(direction).getLocation());
+	    } else {
+	        return Optional.empty();
+	    }
+	}
 
-    public void displayNPCs() {
-        if (npcs.isEmpty()) {
-            // Do nothing
-        } else {
-            npcs.forEach(System.out::println);
-        }
-    }
-
-    public void addDirection(Direction direction) {
+	public void addDirection(Direction direction) {
         directions.put(direction.getDirection(), direction);
     }
 
-    public Optional<String> getLocation(String direction) {
-        if (this.directions.containsKey(direction)) {
-            return Optional.of(this.directions.get(direction).getLocation());
-        } else {
-            return Optional.empty();
-        }
-    }
+    public void displayItems() {
+	    if (items.isEmpty()) {
+	        // Do nothing
+	    } else {
+	        System.out.print("On the ground you see:");
+	        items.stream()
+	                .forEach(item -> {
+	                    System.out.print(" " + item);
+	                });
+	        System.out.println();
+	    }
+	}
 
-    public void displayPaths() {
+	public void displayNPCs() {
+	    if (npcs.isEmpty()) {
+	        // Do nothing
+	    } else {
+	        npcs.forEach(System.out::println);
+	    }
+	}
+
+	public void displayPaths() {
         directions.forEach((way, direction) -> {
             System.out.print("If you go " + way);
             System.out.println(" you can get to " + direction.getLocation());
         });
-    }
-
-    public void displayItems() {
-        if (items.isEmpty()) {
-            // Do nothing
-        } else {
-            System.out.print("On the ground you see:");
-            items.stream()
-                    .forEach(item -> {
-                        System.out.print(" " + item);
-                    });
-            System.out.println();
-        }
     }
 
 }
